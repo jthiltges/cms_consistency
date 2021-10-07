aa2d9c21bd5773a629cb8ac9353b663804aefec3
ivm <ivm@igors-imac.home>
2021-10-07 10:06:59 -0400

    .

--- rucio-config/export_cfg.py
+++ rucio-config/export_cfg.py
@@ -4,14 +4,16 @@ from configparser import ConfigParser
 
 opts, args = getopt.getopt(sys.argv[1:], "s:")
 opts = dict(opts)
-section = opts.get("-s")
+root = opts.get("-s")
 
 client = ConfigClient()
-data = client.get_config(section)
+all_sections = client.get_config()
 
 cfg = ConfigParser()
-cfg.add_section(section)
-for k, v in data.items():
-	
-	cfg.set(section, k, str(v))
+
+for section, section_cfg in data.items():
+    if root and (section == root or section.startswith(root+"."):
+        cfg.add_section(section)
+        for k, v in section_cfg.items():	
+        	cfg.set(section, k, str(v))
 cfg.write(sys.stdout)
